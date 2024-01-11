9a87b852dba920d62ac1ace2d4ca0caddaec116f
David Marchante <davidmarchan@gmail.com>
2024-01-11 00:19:34 -0500

    Counter app

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,44 @@
 use std::io;
 use std::time::Duration;
 
-use crossterm::event;
 use crossterm::event::Event;
 use crossterm::event::KeyCode;
 use crossterm::event::KeyEventKind;
-use crossterm::terminal;
+use crossterm::terminal::EnterAlternateScreen;
+use crossterm::terminal::LeaveAlternateScreen;
 use crossterm::ExecutableCommand;
 use ratatui::prelude::CrosstermBackend;
-use ratatui::style::Stylize;
 use ratatui::widgets::Paragraph;
 use ratatui::Terminal;
 
 fn main() -> io::Result<()> {
-    io::stdout().execute(terminal::EnterAlternateScreen)?;
-    terminal::enable_raw_mode()?;
-    let mut app = Terminal::new(CrosstermBackend::new(io::stdout()))?;
-    app.clear()?;
+    io::stdout().execute(EnterAlternateScreen)?;
+    crossterm::terminal::enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout()))?;
+
+    let mut counter = 0;
 
     loop {
-        app.draw(|frame| {
-            let area = frame.size();
-            frame.render_widget(
-                Paragraph::new("Heddo! ([q] to quit)").white().on_blue(),
-                area,
-            );
+        terminal.draw(|f| {
+            f.render_widget(Paragraph::new(format!("Counter: {counter}")), f.size());
         })?;
 
-        if event::poll(Duration::from_millis(16))? {
-            if let Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    break;
+        if crossterm::event::poll(Duration::from_millis(16))? {
+            if let Event::Key(key) = crossterm::event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') => break,
+                        KeyCode::Char('k') => counter += 1,
+                        KeyCode::Char('j') => counter -= 1,
+                        _ => {}
+                    }
                 }
             }
         }
     }
 
-    io::stdout().execute(terminal::LeaveAlternateScreen)?;
-    terminal::disable_raw_mode()?;
+    io::stdout().execute(LeaveAlternateScreen)?;
+    crossterm::terminal::disable_raw_mode()?;
 
     Ok(())
 }
